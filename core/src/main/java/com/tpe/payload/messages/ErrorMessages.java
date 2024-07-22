7d192f08888d602dd0561ebac2888941253d487e
aslihankaren <fatmacinaryalcin@hotmail.com>
2024-07-22 19:01:27 +0200

    car delete methodu yazildi

--- core/src/main/java/com/tpe/payload/messages/ErrorMessages.java
+++ core/src/main/java/com/tpe/payload/messages/ErrorMessages.java
@@ -2,19 +2,19 @@ package com.tpe.payload.messages;
 
 
 public class ErrorMessages {
-
-
-    public static final String RESERVATION_DOES_NOT_EXISTS_BY_ID = "Reservation not found";
-
     private ErrorMessages() {
     }
 
+
     public static final String CAR_EXISTS_BY_THIS_NUMBERPLATE = "Error: Car is already exists with this number plate %s";
     public static final String CAR_DOES_NOT_EXISTS_BY_ID= "Error: Car is not exists by id: %s";
+
+    public static final String CAR_CANNOT_DELETED="Error! Car can not be deleted because the car is not available";
 
     public static final String LOG_NOT_CREATED = "Error: Log is not created.";
 
     public static final String RESERVATION_NOT_AVAILABLE = "Car with ID %d is not available for the specified time range";
 
+    public static final String RESERVATION_DOES_NOT_EXISTS_BY_ID = "Reservation not found";
 
 }
